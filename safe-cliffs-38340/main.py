27f0e58dd83cf413b287f9cdb3743021416009fd
matu <matu@mail>
2018-07-24 21:04:40 +0900

    aaaa

--- safe-cliffs-38340/main.py
+++ safe-cliffs-38340/main.py
@@ -63,10 +63,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+	line_bot_api.reply_message(reply_token, TextSendMessage(text='Hello World!'))
+#    line_bot_api.reply_message(
+#        event.reply_token,
+#        TextSendMessage(text=event.message.text)
+#    )
 
 
 if __name__ == "__main__":
